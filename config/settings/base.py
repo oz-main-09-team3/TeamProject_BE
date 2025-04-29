44a2a989ab911ca0cc986f0194e8a9d2ccb4c0e7
SWWS97 <parkmon2@naver.com>
2025-04-29 13:17:20 +0900

    배포 환경 테스트

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ import random
 from dotenv import dotenv_values
 
 # dotenv_values : env 파일의 경로를 파라미터로 전달 받아 해당 파일을 읽어온 후 key, value 형태로 매핑하여 dict로 반환한다.
-ENV = dotenv_values("../.env/local.env")
+ENV = dotenv_values("../envs/dev.env")
 
 
 from pathlib import Path
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,8 @@
-from config.settings.base import *
+from .base import *
+
 
 DEBUG = True
 ALLOWED_HOSTS = []
+
+# dotenv_values : env 파일의 경로를 파라미터로 전달 받아 해당 파일을 읽어온 후 key, value 형태로 매핑하여 dict로 반환한다.
+ENV = dotenv_values("../envs/dev.env")
