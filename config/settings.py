8a6cb7e4c45bb074909237b86738f56ff8ad8ef0
Baehyeonu <wea3113@gmail.com>
2025-05-02 13:40:31 +0900

    docs:settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "users.User"
 # Application definition
