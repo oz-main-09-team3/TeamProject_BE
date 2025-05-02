7f5607fcd2b6fb61bbc6d8adcd4683493a44d650
SWWS97 <parkmon2@naver.com>
2025-05-02 16:01:43 +0900

    CI/CD 환경 추가

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,18 +1,20 @@
 from .base import *
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env") # 루트의 .env 로드
 
 DEBUG = True
 ALLOWED_HOSTS = []
 
-# dotenv_values : env 파일의 경로를 파라미터로 전달 받아 해당 파일을 읽어온 후 key, value 형태로 매핑하여 dict로 반환한다.
-ENV = dotenv_values("../envs/dev.env")
 
-DATABASES = {
-    "default": {
+DATABASES["default"].update({
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": ENV.get("POSTGRES_DB"),  # dev.env에 있는 DB 이름
-        "USER": ENV.get("POSTGRES_USER"),  # dev.env에 있는 해당 DB 유저
-        "PASSWORD": ENV.get("POSTGRES_PASSWORD"),  # dev.env에 있는 DB 패스워드
-        "HOST": ENV.get("POSTGRES_HOST"),  # dev.env에 있는 DB HOST
-        "PORT": ENV.get("POSTGRES_PORT"),  # dev.env에 있는 DB PORT
-    }
-}
+        "NAME": os.getenv("POSTGRES_DB"),  # dev.env에 있는 DB 이름
+        "USER": os.getenv("POSTGRES_USER"),  # dev.env에 있는 해당 DB 유저
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),  # dev.env에 있는 DB 패스워드
+        "HOST": os.getenv("POSTGRES_HOST"),  # dev.env에 있는 DB HOST
+        "PORT": os.getenv("POSTGRES_PORT"),  # dev.env에 있는 DB PORT
+})
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,20 +1,21 @@
 from .base import *
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env") # 루트의 .env 로드
 
 DEBUG = False
 ALLOWED_HOSTS = ["43.201.23.196"]
-# dotenv_values("../envs/prod.env") 로 실제 DB/SECRET_KEY 읽어오기
 
 
-# dotenv_values : env 파일의 경로를 파라미터로 전달 받아 해당 파일을 읽어온 후 key, value 형태로 매핑하여 dict로 반환한다.
-ENV = dotenv_values("../envs/prod.env")
-
-DATABASES = {
-    "default": {
+DATABASES["default"].update({
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": ENV.get("POSTGRES_DB"),  # prod.env에 있는 DB 이름
-        "USER": ENV.get("POSTGRES_USER"),  # prod.env에 있는 해당 DB 유저
-        "PASSWORD": ENV.get("POSTGRES_PASSWORD"),  # prod.env에 있는 DB 패스워드
-        "HOST": ENV.get("POSTGRES_HOST"),  # prod.env에 있는 DB HOST
-        "PORT": ENV.get("POSTGRES_PORT"),  # prod.env에 있는 DB PORT
-    }
-}
+        "NAME": os.getenv("POSTGRES_DB"),  # prod.env에 있는 DB 이름
+        "USER": os.getenv("POSTGRES_USER"),  # prod.env에 있는 해당 DB 유저
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),  # prod.env에 있는 DB 패스워드
+        "HOST": os.getenv("POSTGRES_HOST"),  # prod.env에 있는 DB HOST
+        "PORT": os.getenv("POSTGRES_PORT"),  # prod.env에 있는 DB PORT
+})
+
