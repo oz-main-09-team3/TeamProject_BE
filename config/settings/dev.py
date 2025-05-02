e001fed719fb742df3832a2ab67e0cada7c1eaa7
SWWS97 <parkmon2@naver.com>
2025-05-02 16:02:50 +0900

    CI/CD 환경 추가

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,20 +1,24 @@
-from .base import *
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from .base import *
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env") # 루트의 .env 로드
+load_dotenv(BASE_DIR / ".env")  # 루트의 .env 로드
 
 DEBUG = True
 ALLOWED_HOSTS = []
 
 
-DATABASES["default"].update({
+DATABASES["default"].update(
+    {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),  # dev.env에 있는 DB 이름
         "USER": os.getenv("POSTGRES_USER"),  # dev.env에 있는 해당 DB 유저
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),  # dev.env에 있는 DB 패스워드
         "HOST": os.getenv("POSTGRES_HOST"),  # dev.env에 있는 DB HOST
         "PORT": os.getenv("POSTGRES_PORT"),  # dev.env에 있는 DB PORT
-})
+    }
+)
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,21 +1,24 @@
-from .base import *
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from .base import *
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env") # 루트의 .env 로드
+load_dotenv(BASE_DIR / ".env")  # 루트의 .env 로드
 
 DEBUG = False
 ALLOWED_HOSTS = ["43.201.23.196"]
 
 
-DATABASES["default"].update({
+DATABASES["default"].update(
+    {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),  # prod.env에 있는 DB 이름
         "USER": os.getenv("POSTGRES_USER"),  # prod.env에 있는 해당 DB 유저
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),  # prod.env에 있는 DB 패스워드
         "HOST": os.getenv("POSTGRES_HOST"),  # prod.env에 있는 DB HOST
         "PORT": os.getenv("POSTGRES_PORT"),  # prod.env에 있는 DB PORT
-})
-
+    }
+)
