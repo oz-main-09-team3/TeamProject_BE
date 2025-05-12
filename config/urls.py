96f687960d8d583d2fb9b47893e74d71ce00c288
nojimin <sh7606555@gmail.com>
2025-05-12 17:37:03 +0900

    SCRUM-19 API Swagger 연결

--- config/urls.py
+++ config/urls.py
@@ -17,23 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.views.generic import TemplateView
 
 from apps import diary
 from users.views import LogoutAPIView, OAuthLoginView, UserMeAPIView
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Emotion API",
-        default_version="v1",
-        description="감정 관련 API 문서입니다.",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -46,12 +33,6 @@ urlpatterns = [
     path("api/diary/", include("apps.diary.urls")),
     path("api/qrcode/", include("qr.urls")),
     path("api/notifications/", include("notifications.urls")),
-    # Swagger UI
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    # Redoc UI (선택)
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # API-Swagger
+    path("swagger/", TemplateView.as_view(template_name="swagger.html")),
 ]
