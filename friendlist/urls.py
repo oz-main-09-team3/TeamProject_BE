a990cf7f7d961a55c7d42ce8669cbc27c1671b64
Baehyeonu <wea3113@gmail.com>
2025-05-16 13:04:41 +0900

    SCRUM-16-FriendList 로직 전체 수정

--- friendlist/urls.py
+++ friendlist/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import FriendDeleteAPIView, FriendListAPIView
+from .views import (
+    FriendDeleteAPIView,
+    FriendListAPIView,
+)
 
 urlpatterns = [
+    # 친구 목록 조회: GET  /api/friends/?status=…
     path("", FriendListAPIView.as_view(), name="friend-list"),
+    # 친구 삭제:      DELETE /api/friends/<int:friend_id>/
     path("<int:friend_id>/", FriendDeleteAPIView.as_view(), name="friend-delete"),
 ]
