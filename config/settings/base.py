a6b426ee51805d56f2a8fc953f195d0b266aa282
SWWS97 <parkmon2@naver.com>
2025-05-20 04:03:55 +0900

    SCRUM-26-CloudFront

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,8 +186,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -242,5 +242,8 @@ STORAGES = {
     },
 }
 
-AWS_S3_CUSTOM_DOMAIN = f"{os.getenv("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_CLOUDFRONT_DOMAIN")
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
